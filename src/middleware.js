eb7963d2b9ad4901afcbf1736303c79c27e7a962
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 16:18:18 +0900

    Feat : middleware를 실행시키는 run 함수 추가
    run 함수에서 request, response 를 저장한 후 runMiddleware 실행
    runMiddleWare는 재귀적으로 동작

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,26 @@
 const middleware = () => {
     const middlewares = [];
-  
+    const request, response;
+
     const add = (func) => {
         middleware.push(func)
+    }
+
+    const run = (req, res) => {
+        request = req;
+        response = res;
+        runMiddleWare(0);
+    }
+
+    const runMiddleWare = (index) => {
+        if (index < 0 || middlewares.length < index) {
+            return;
+        }
+
+        const nextMiddleWare = middlewares[index];
+        const next = () => runMiddleWare(index+1);
+
+        nextMiddleWare(request, response, next);
     }
 
 
