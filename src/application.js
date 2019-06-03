c8fa7cd9f0d7ec27fb0e52660fe81cb8c0b28bb0
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 16:01:25 +0900

    Feat : serve-static 모듈 적용해 페이지 요청에 응답
    todo : 응답 헤더를 2번 보내는 문제 해결 필요

--- src/application.js
+++ src/application.js
@@ -1,19 +1,25 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const serveStatic = require('./serve-static')
+
 const debug = require('../utils/debug')('application');
 
 const application = () => {
 
     const server = http.createServer((req, res) => {
-        const filePath = path.join(__dirname, '../public/index.html');
-        fs.readFile(filePath, (err, data) => {
-            if (err) throw err;
+        serveStatic(req, res);
 
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html') 
-            res.end(data);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+      
+        const filePath = path.join(__dirname, '../public/index.html')
+        fs.readFile(filePath, (err, data) => {
+          if (err) throw err;
+      
+          res.end(data);
         })
+      
     });
 
     const listen = (host, port, func) => {
