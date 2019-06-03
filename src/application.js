b5a897773c87df00c1c7ce7e493cc911db45c7a8
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 20:31:02 +0900

    Feat : get 메서드 요청 지원하는 get 함수 추가

--- src/application.js
+++ src/application.js
@@ -1,22 +1,17 @@
 const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const serveStatic = require('../middlewares/serve-static');
 const Middleware = require('./middleware');
 const Response = require('./resonse');
-
+const Request = require('./Request');
 const debug = require('../utils/debug')('application');
 
 const application = () => {
-
     const middleware = Middleware();
 
     const server = http.createServer((req, res) => {
-        middleware.run(req, Response(res));
+        middleware.run(Request(req), Response(res));
     });
 
     const use = (path, func) => {
-
         if (typeof path === 'string' && typeof func === 'function') {
             func.path = path;
         } else if (typeof path == 'function') {
@@ -24,8 +19,13 @@ const application = () => {
         } else {
             throw Error('Usage: use(path, fn) or use(fn)');
         }
-
         middleware.add(func);
+    }
+
+    const get = (path, func) => {
+        if (!path || !func) throw Error('path and fn is required')
+        func.method = 'GET';
+        use(path, func);
     }
 
     const listen = (host, port, func) => {
