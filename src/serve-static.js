c103544e391e420cc46b3b7f7bd4df9fb78a2328
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 16:00:24 +0900

    Create : 동적으로 정적 파일 타입과 헤더를 제공하는 serve-static 생성
    동적으로 파일 확장자를 추출해 응답 객체의 헤더 정보 생성

---
+++ src/serve-static.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+
+const serveStatic = (req, res) => {
+  const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
+  }
+
+  const ext = path.parse(req.url).ext;
+  console.log(`확장자는 ${ext}`);
+  const publicPath = path.join(__dirname, '../public')
+
+  if (Object.keys(mimeType).includes(ext)) {
+    fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('Not found page');
+      }
+
+      res.statusCode = 200
+      res.setHeader('Content-Type', mimeType[ext]);
+      res.end(data)
+
+    })
+  } else {
+    res.statusCode = 200;
+  }
+};
+
+module.exports = serveStatic;
