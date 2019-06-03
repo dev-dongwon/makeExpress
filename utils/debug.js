a0778f9b759877d1dfd38a964653c4bcbb9836e7
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 15:23:22 +0900

    Feat : 디버그 대상 모듈에 색상 추가
    디버그 대상 모듈은 보라색으로 출력되도록 색상 추가

--- utils/debug.js
+++ utils/debug.js
@@ -2,7 +2,7 @@ const debug = (tag) => {
     if (!tag) throw Error('tag should be required');
     
     return (message) => {
-        const logString = `[${tag}] : ${message}`;
+        const logString = '\x1b[35m' + `[${tag}]` + '\x1b[0m' +  `: ${message}`;
         console.log(logString);
         return logString;
     }
