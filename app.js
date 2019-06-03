1c2fc51468bf6f45b1fd1195f18c8a253a56cc45
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 18:17:11 +0900

    Fix : 미들웨어 인터페이스에 맞춰 함수 wrapping

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const App = require('./src/application');
-const path = require('path');
 const debug = require('./utils/debug')('app');
 const serveStatic = require('./middlewares/serve-static');
-const fs = require('fs');
-const app = App();
 const logger = require('./middlewares/logger');
 const errors = require('./middlewares/errors');
+const index = require('./routers/index');
 
-
-const index = (req, res, next) => {
-    const publicPath = path.join(__dirname, './public')
-  
-    fs.readFile(`${publicPath}/index.html`, (err, data) => {
-      if (err) throw err
-  
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'text/html')
-      res.end(data)
-    })
-  };
-  
+const app = App();
 
 app.use(logger());
 app.use(serveStatic());
-app.use(index);
+app.use('/', index());
 app.use(errors.error());
 app.use(errors.error404());
 
--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,11 +1,11 @@
-const error404 = (req, res, next) => {
-    res.statusCode = 404
-    res.end('Not Found')
+const error404 = () => (req, res, next) => {
+    res.statusCode = 404;
+    res.end('Not Found');
 }
 
-const error = (err, req, res, next) => {
-    res.statusCode = 500
-    res.end()
+const error = () => (err, req, res, next) => {
+    res.statusCode = 500;
+    res.end();
 }
 
 module.exports = {
