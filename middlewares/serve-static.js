a1bb9fcf899d830b67d19a459b3284fdd23d0f2a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 16:47:04 +0900

    Update : 미들웨어 인터페이스 구현하도록 next 인자 추가

--- middlewares/serve-static.js
+++ middlewares/serve-static.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const serveStatic = (req, res, next) => {
+const serveStatic = () => (req, res, next) => {
   const mimeType = {
     '.ico': 'image/x-icon',
     '.html': 'text/html',
