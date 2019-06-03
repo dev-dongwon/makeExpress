b90e486608fc83467dd4333b4046d794ecbc0535
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 17:21:25 +0900

    Feat : serveStatic 미들웨어 추가

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const App = require('./src/application');
-const app = App();
+const path = require('path');
 const debug = require('./utils/debug')('app');
+const serveStatic = require('./middlewares/serve-static');
+const fs = require('fs');
+const app = App();
+
+const index = (req, res, next) => {
+    const publicPath = path.join(__dirname, './public')
+  
+    fs.readFile(`${publicPath}/index.html`, (err, data) => {
+      if (err) throw err
+  
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html')
+      res.end(data)
+    })
+  };
+  
+  const error404 = (req, res, next) => {
+    res.statusCode = 404
+    res.end('Not Found')
+  }
+  
+  const error = (err, req, res, next) => {
+    res.statusCode = 500
+    res.end()
+  }
+
+app.use(serveStatic());
+app.use(index);
+app.use(error404)
+app.use(error)
 
 debug('app is initiated');
 module.exports = app;
