12b0338b057d48f952c12cb80f5a0f2d9ac8b47e
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-03 15:40:48 +0900

    Fix : http 헤더값에 statusCode와 Content-type 추가
    Content-type header를 text/plain에서 text/html로 변경

--- application.js
+++ application.js
@@ -5,10 +5,13 @@ const debug = require('./utils/debug')('application');
 
 const application = () => {
     const server = http.createServer((req, res) => {
+
         const filePath = path.join(__dirname, '/public/index.html');
         fs.readFile(filePath, (err, data) => {
             if (err) throw err;
 
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/html') 
             res.end(data);
         })
     });
