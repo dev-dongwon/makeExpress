8988c3edd1db5cd8e233037196c1a5cbe928c8cf
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-05 15:48:06 +0900

    Create : 회원가입 router 추가
    회원가입 페이지를 send 하는 라우터 추가

---
+++ routers/join.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
+const join = () => (req, res, next) => {
+    const publicPath = path.join(__dirname, '../public')
+
+    fs.readFile(`${publicPath}/join.html`, (err, data) => {
+        if (err) throw err
+        res.status(200).set('Content-Type', 'text/html').send(data);
+    })
+};
+
+module.exports = join;
